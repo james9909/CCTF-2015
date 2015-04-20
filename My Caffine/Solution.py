daa5397fb2d5b0bbfc40ebf91051e5ece669b344
James Wang <jameswang9909@hotmail.com>
2015-04-20 16:10:18 -0400

    Solve My Caffine

---
+++ My Caffine/Solution.py
@@ -0,0 +1,24 @@
+from requests import Request, Session
+import subprocess
+
+HEADERS={"Content-Type":"application/coffee-pot-command", "cookie":"PHPSESSID=oc5pnc39qghan8gb8ve96uhs10"}
+session = Session()
+SERVER="http://coffee.camsctf.com/"
+
+# Brew dat coffee
+req = Request('BREW',  SERVER, headers=HEADERS, data="23")
+prep_request = session.prepare_request(req)
+output = session.send(prep_request)
+print output.text
+
+# After this we get some mad weird stuff
+req=Request('WHEN',  SERVER, headers=HEADERS)
+prep_request = session.prepare_request(req)
+output = session.send(prep_request)
+print output.text
+
+# Refresh
+req = Request('PROPFIND',  SERVER, headers=HEADERS)
+prep_request = session.prepare_request(req)
+output = session.send(prep_request)
+print output.text
