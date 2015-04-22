66772cab9ccf10f91dd8a9204311ed5cb039bf10
James Wang <jameswang9909@hotmail.com>
2015-04-21 22:59:36 -0400

    Solve Stream 2

--- Stream 2/Solution.bat
+++ Stream 2/Solution.bat
@@ -1,9 +1,6 @@
-CD C:\Users\ABCD\Desktop\cctf
-
-
 SET /A number=0
 :while1
-	IF NOT %number% == 100 (
+	IF NOT %number% == 4820 (
 		SET /A number=number+1
 		CAT secret2.docx:flag%number%.txt
 		GOTO :while1
